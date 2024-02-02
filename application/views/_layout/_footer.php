f22b0c3c483a4d66ea447b450bf998159e14168c
unknown <azzahrarn7@gmail.com>
2024-02-02 13:15:43 +0700

    komentar

---
+++ application/views/_layout/_footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+		<strong>Version</strong> 3.1.0
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright &copy; 2014-2018 <a href="#">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
